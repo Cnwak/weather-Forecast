16de83200e9390869580310fbbf54419c24292b5
Chijindu <cnnwakama@gmail.com>
2020-10-27 11:55:55 +0100

    Improved UI

--- ui.js
+++ ui.js
@@ -56,11 +56,11 @@ class UI{
     // 1  
     
     
-    this.temp1.textContent= `${details.list[0].main.temp}°c`
+    this.temp1.innerHTML= `<i class="fa fa-thermometer-quarter fa-4x blue-text darken-2" aria-hidden="true"></i><h2 id="temp" class="margin">${details.list[0].main.temp}°c</h2> `
     this.icon1.setAttribute('src',`http://openweathermap.org/img/w/${details.list[0].weather[0].icon}.png`)
     this.description1.textContent= `${details.list[0].weather[0].description}`
     this.humidity1.textContent= `Humidity: ${details.list[0].main.humidity}%`
-    this.wind1.textContent=`Wind speed: ${details.list[0].wind.speed}km/h`
+    this.wind1.innerHTML=`<i class="fas fa-wind"></i>:  ${details.list[0].wind.speed}km/h`
     this.main1.textContent= `${details.list[0].weather[0].main}`
     this.date1.textContent= new Date(details.list[0].dt_txt).toDateString()
     this.cityName.textContent= `${details.city.name},${details.city.country}`
@@ -68,41 +68,41 @@ class UI{
 
     // 2
     
-    this.temp2.textContent= `${details.list[6].main.temp}°c`
+    this.temp2.innerHTML= `<i class="fa fa-thermometer-quarter fa-4x light-blue-text darken-2" aria-hidden="true"></i><h2 id="temp" class="margin">${details.list[6].main.temp}°c</h2> `
     this.icon2.setAttribute('src',`http://openweathermap.org/img/w/${details.list[6].weather[0].icon}.png`)
     this.description2.textContent= `${details.list[6].weather[0].description}`
     this.humidity2.textContent= `Humidity: ${details.list[6].main.humidity}%`
-    this.wind2.textContent= `Wind speed: ${details.list[6].wind.speed}km/h`
+    this.wind2.innerHTML= `<i class="fas fa-wind"></i>:   ${details.list[6].wind.speed}km/h`
     this.main2.textContent= `${details.list[6].weather[0].main}`
     this.date2.textContent= new Date(details.list[6].dt_txt).toDateString()
   
 
     //3
-    this.temp3.textContent= `${details.list[14].main.temp}°c`
+    this.temp3.innerHTML= `<i class="fa fa-thermometer-quarter fa-4x cyan-text darken-2" aria-hidden="true"></i><h2 id="temp" class="margin">${details.list[14].main.temp}°c</h2> `
     this.icon3.setAttribute('src',`http://openweathermap.org/img/w/${details.list[18].weather[0].icon}.png`)
     this.description3.textContent= `${details.list[18].weather[0].description}`
     this.humidity3.textContent= `Humidity: ${details.list[18].main.humidity}%`
-    this.wind3.textContent= `Wind speed: ${details.list[18].wind.speed}km/h`
+    this.wind3.innerHTML= `<i class="fas fa-wind"></i>:   ${details.list[18].wind.speed}km/h`
     this.main3.textContent= `${details.list[18].weather[0].main}`
     this.date3.textContent= new Date(details.list[18].dt_txt).toDateString()
 
 
     //4
-    this.temp4.textContent= `${details.list[22].main.temp}°c`
+    this.temp4.innerHTML= `<i class="fa fa-thermometer-quarter fa-4x teal-text darken-2" aria-hidden="true"></i><h2 id="temp" class="margin">${details.list[22].main.temp}°c</h2> `
     this.icon4.setAttribute('src',`http://openweathermap.org/img/w/${details.list[22].weather[0].icon}.png`)
     this.description4.textContent= `${details.list[22].weather[0].description}`
     this.humidity4.textContent= `Humidity: ${details.list[22].main.humidity}%`
-    this.wind4.textContent= `Wind speed: ${details.list[22].wind.speed}km/h`
+    this.wind4.innerHTML= `<i class="fas fa-wind"></i>: ${details.list[22].wind.speed}km/h`
     this.main4.textContent= `${details.list[22].weather[0].main}`
     this.date4.textContent= new Date(details.list[22].dt_txt).toDateString()
 
 
     //5
-    this.temp5.textContent= `${details.list[35].main.temp}°c`
+    this.temp5.innerHTML= `<i class="fa fa-thermometer-quarter fa-4x indigo-text darken-2" aria-hidden="true"></i><h2 id="temp" class="margin">${details.list[35].main.temp}°c</h2> `
     this.icon5.setAttribute('src',`http://openweathermap.org/img/w/${details.list[35].weather[0].icon}.png`)
     this.description5.textContent= `${details.list[35].weather[0].description}`
     this.humidity5.textContent= `Humidity: ${details.list[35].main.humidity}%`
-    this.wind5.textContent= `Wind speed: ${details.list[35].wind.speed}km/h`
+    this.wind5.innerHTML= `<i class="fas fa-wind"></i>: ${details.list[35].wind.speed}km/h`
     this.main5.textContent= `${details.list[35].weather[0].main}`
     this.date5.textContent= new Date(details.list[35].dt_txt).toDateString()
   }
